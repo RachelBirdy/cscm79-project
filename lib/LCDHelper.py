1545064bd482b9134e3fd9f424299955e1301dab
Rachel Birdy <rachel@weird.tf>
2024-03-16 15:59:56 +0000

    Update LCD helper

--- lib/LCDHelper.py
+++ lib/LCDHelper.py
@@ -1,22 +1,21 @@
 from Phidget22.Phidget import *
 from Phidget22.Devices.LCD import *
 
-lcd0 = None
-setupComplete = False
 
-def setup(serialNo):
+def setup(serialNo, isRemote = 0):
     lcd0 = LCD()
     lcd0.setChannel(0)
     lcd0.setDeviceSerialNumber(serialNo)
+    lcd0.setIsRemote(isRemote)
     lcd0.openWaitForAttachment(5000)
-    setupComplete=True
+    lcd0.setBacklight(1)
     return lcd0
 
-def write(Line1, Line2):
-    if(setupComplete):
-        lcd0.clear()
-        lcd0.writeText(LCDFont.FONT_5x8, 0, 0, Line1)
-        lcd0.writeText(LCDFont.FONT_5x8, 0, 1, Line2)
-        lcd0.flush()
-    return
+def write(lcd0, Line1, Line2):
+    lcd0.clear()
+    lcd0.writeText(LCDFont.FONT_5x8, 0, 0, Line1)
+    lcd0.writeText(LCDFont.FONT_5x8, 0, 1, Line2)
+    lcd0.flush()
 
+def setdown(lcd0):
+    lcd0.close()
