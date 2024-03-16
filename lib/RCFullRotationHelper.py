b8c236dbc8a0a9181ee48090b2f7faf9e9c5cbab
Rachel Birdy <rachel@weird.tf>
2024-03-16 16:00:09 +0000

    Update RC helper

--- lib/RCFullRotationHelper.py
+++ lib/RCFullRotationHelper.py
@@ -1,14 +1,10 @@
 from Phidget22.Phidget import *
 from Phidget22.Devices.RCServo import *
 
-#rcServo0 = None
-setupComplete = False
 Neutral = 91
 MaxRot = 180
 
 def setup(serialNo, isRemote = 0):
-    global rcServo0
-    global setupComplete
     rcServo0 = RCServo()
     rcServo0.setDeviceSerialNumber(serialNo)
     rcServo0.setChannel(0)
@@ -16,28 +12,18 @@ def setup(serialNo, isRemote = 0):
     rcServo0.openWaitForAttachment(5000)
     rcServo0.setTargetPosition(0)
     rcServo0.setEngaged(True)
-    setupComplete = True
-    return
+    return rcServo0
 
-def rotate(speed:float):
+def rotate(rcServo0, speed:float):
     if((speed > 1) | (speed < -1)):
         raise ValueError("Speed must be between 1.0 ad -1.0")
-    if(setupComplete):
-        rcServo0.setTargetPosition(Neutral + (MaxRot - Neutral)*speed)
+    rcServo0.setTargetPosition(Neutral + (MaxRot - Neutral)*speed)
 
-def rotClockwise(distance):
-    if(setupComplete):
-        rcServo0.setTargetPosition(90+distance)
+def rotClockwise(rcServo0, distance):
+    rcServo0.setTargetPosition(90+distance)
 
-def rotAntiClockwise(distance):
-    global setupComplete
-    if(setupComplete):
-        rcServo0.setTargetPosition(90-distance)
+def rotAntiClockwise(rcServo0, distance):
+    rcServo0.setTargetPosition(90-distance)
 
-def setdown():
-    global setupComplete
-    global rcServo0
-    if(setupComplete):
-        rcServo0.close()
-        setupComplete = False
-        rcServo0 = None
+def setdown(rcServo0):
+    rcServo0.close()
