e56f1c5fb4019066491897914827e856df30718a
RachelBirdy <rachel@weird.tf>
2024-04-30 16:20:17 +0100

    mmm

--- raspi.py
+++ raspi.py
@@ -3,6 +3,24 @@ from Phidget22.Devices.Dictionary import *
 from Phidget22.Net import *
 from Phidget22.Devices.Manager import *
 from time import *
+from lib import LCDHelper as LCD
+from lib import RCFullRotationHelper as RC
+from lib import TempLEDHelper as LED
+from lib import VoltageInputHelper as VI
+from lib import RFIDHelper as RF
+
+TempControl = False
+RFIDtag = False
+BarrierControl = False
+SwingControl = False
+LCDConnected = False
+
+lcd0 = None
+leds = None
+voltage0 = None
+rfid0 = None
+barrierMotor0 = None
+swingMotor0 = None
 
 def onServerAdded(self, server, kv):
     print("Server: " + str(server))
@@ -10,14 +28,26 @@ def onServerAdded(self, server, kv):
 
 def onNewDevice(self, device):
     print("Device: " + str(device))
+    if (device.getDeviceSerialNumber() == 39830): LCDConnected = True
+    if (device.getDeviceSerialNumber() == 63514): RFIDtag = True
+    if (device.getDeviceSerialNumber() == 14875): BarrierControl = True
+    if (device.getDeviceSerialNumber() == 19875): SwingControl = True
+
+RGBpins = {
+    "Red": 7,
+    "Green": 6,
+    "Blue": 5
+}
 
 def main():
+
+
     net = Net()
     net.setOnServerAddedHandler(onServerAdded)
     Net.enableServerDiscovery(PhidgetServerType.PHIDGETSERVER_DEVICEREMOTE)
     sleep(5)
     manager = Manager()
-    manager.setOnAttachHandler(onNewDevice)
+    manager.setOnAttachHandle(onNewDevice)
     manager.open()
     cribDict = Dictionary()
     cribDict.setDeviceLabel("Crib")
@@ -26,5 +56,8 @@ def main():
     cribDict.set("test", "69")
     print(cribDict.get("test"))
     cribDict.close()
-    
+
+    while(True):
+        time.sleep(1)
+
 main()
